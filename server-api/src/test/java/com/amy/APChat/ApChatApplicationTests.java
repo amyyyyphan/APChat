a75728fd3258c97a5b94eaab92616a7c2d80e706
Amy Phan <amyyyyphan99@gmail.com>
2024-03-06 18:38:03 -0800

    first commit

---
+++ server-api/src/test/java/com/amy/APChat/ApChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amy.APChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
