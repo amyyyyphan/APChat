d2b760f3c7752af3b86861c8a29da3fc139c0145
Amy Phan <amyyyyphan99@gmail.com>
2024-03-07 20:38:25 -0800

    add chat API

---
+++ server-api/src/main/java/com/amy/APChat/enums/MessageType.java
@@ -0,0 +1,5 @@
+package com.amy.APChat.enums;
+
+public enum MessageType {
+    CHAT, CONNECT, DISCONNECT
+}
---
+++ server-api/src/main/java/com/amy/APChat/model/Message.java
@@ -0,0 +1,15 @@
+package com.amy.APChat.model;
+
+import com.amy.APChat.enums.MessageType;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+    private MessageType type;
+    private String topic;
+    private String sender;
+    private String content;
+}
