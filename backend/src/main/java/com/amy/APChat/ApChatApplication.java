8b0fe32a87c37b2b59ffe744633e2bde55256f27
Amy Phan <amyyyyphan99@gmail.com>
2024-03-17 02:07:29 -0700

    implement kafka consumer

---
+++ backend/src/main/java/com/amy/APChat/ApChatApplication.java
@@ -0,0 +1,13 @@
+package com.amy.APChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApChatApplication.class, args);
+	}
+
+}
