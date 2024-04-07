d4c69485d986772bf66f53f8bfc10adcc97a0487
Amy Phan <39923167+amyyyyphan@users.noreply.github.com>
2024-04-07 00:02:15 -0700

    add home page

---
+++ frontend/src/constants.js
@@ -0,0 +1,8 @@
+export const TOPICS = {
+    computerScience: "computer-science",
+    engineering: "engineering",
+    food: "food",
+    math: "math",
+    movies: "movies",
+    sports: "sports",
+};
