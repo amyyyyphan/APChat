109a6d13380410052179f8d569574573b7cce553
Amy Phan <amyyyyphan99@gmail.com>
2024-04-16 19:39:56 -0700

    update chat page to display messages

--- backend/src/main/java/com/amy/APChat/consumer/KafkaConsumer.java
+++ backend/src/main/java/com/amy/APChat/consumer/KafkaConsumer.java
@@ -1,15 +1,20 @@
 package com.amy.APChat.consumer;
 
 import com.amy.APChat.model.Message;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KafkaConsumer {
+    @Autowired
+    SimpMessagingTemplate template;
 
     @KafkaListener(topics = "${kafka.topic-1}", groupId = "${kafka.consumer.group-id-1}")
     public void listen(Message message) {
-        System.out.println(message.getContent());
+        System.out.println(message.getSender() + ": " + message.getContent());
+        template.convertAndSend("/chat/food", message);
     }
 
 }
--- frontend/src/pages/FoodChatroomPage/FoodChatroomPage.js
+++ frontend/src/pages/FoodChatroomPage/FoodChatroomPage.js
@@ -1,10 +1,12 @@
 import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Client } from "@stomp/stompjs";
 import axios from "axios";
-import apiUrl from "../../config";
+import config from "../../config";
 
 import TopBar from "../../components/TopBar/TopBar";
+import ChatMessageCard from "../../components/ChatMessageCard/ChatMessageCard";
 
 import { TOPICS, MESSAGE_TYPE } from "../../constants";
 
@@ -14,31 +16,58 @@ const FoodChatroomPage = () => {
   const navigate = useNavigate();
 
   const userTag = useSelector((state) => state.userTag);
+  const [messages, setMessages] = useState([]);
   const [message, setMessage] = useState("");
+
+  const client = new Client({
+    brokerURL: config.webSocketUrl,
+    onConnect: () => {
+      client.subscribe("/chat/food", (frame) => {
+        const chatMessage = JSON.parse(frame.body);
+        console.log(chatMessage);
+        setMessages(messages => [
+          ...messages,
+          chatMessage,
+        ]);
+      });
+    },
+  });
 
   useEffect(() => {
     if (!userTag) {
       navigate("/create-user-tag");
     }
+
+    client.activate();
+
+    return () => {
+      client.deactivate();
+    }
   }, [userTag]);
   
   const handleClick = () => {
-    axios.post(`${apiUrl}/chat`, {
+    axios.post(`${config.apiUrl}/chat`, {
       type: MESSAGE_TYPE.chat,
       topic: TOPICS.food,
       sender: userTag,
       content: message,
     })
-    .then((response) => {
+    .then(() => {
       setMessage("");
-    }), (error) => {
+    })
+    .catch((error) => {
       console.log(error);
-    }
+    })
   }
 
   return (
     <div className={styles.container}>
       <TopBar topic={TOPICS.food}/>
+      <div className={styles.mainContainer}>
+        {messages.map((message) => (
+          <ChatMessageCard message={message} />
+        ))}
+      </div>
       <div className={styles.messageInput}>
         <input
           required
