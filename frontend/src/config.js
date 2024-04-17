16119d8f491a602bd19f5d2ffbf30319cd3a6463
Amy Phan <39923167+amyyyyphan@users.noreply.github.com>
2024-04-16 19:43:25 -0700

    update chat page to display messages

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,6 @@
-const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:8080";
+const config = {
+  apiUrl: process.env.REACT_APP_API_URL || "http://localhost:8080",
+  webSocketUrl: process.env.REACT_APP_WEBSOCKET_URL || "ws://localhost:8081/ws-chat",
+};
 
-export default apiUrl;
+export default config;
