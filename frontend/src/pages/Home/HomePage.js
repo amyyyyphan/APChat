e4862ec23e1c4295db07eedb24e2f3b4703ccee1
Amy Phan <amyyyyphan99@gmail.com>
2024-04-27 22:50:13 -0700

    restyle home page

--- frontend/src/pages/Home/HomePage.js
+++ frontend/src/pages/Home/HomePage.js
@@ -8,12 +8,12 @@ const HomePage = () => {
     <div className={styles.container}>
       <h1>APChat</h1>
       <div className={styles.selectPrompt}>Select a chatroom to join:</div>
-      <TopicButton topic={TOPICS.computerScience}/>
-      <TopicButton topic={TOPICS.engineering}/>
-      <TopicButton topic={TOPICS.food}/>
-      <TopicButton topic={TOPICS.math}/>
-      <TopicButton topic={TOPICS.movies}/>
-      <TopicButton topic={TOPICS.sports}/>
+      <TopicButton topic={TOPICS.computerScience} text="Computer Science"/>
+      <TopicButton topic={TOPICS.engineering} text="Engineering"/>
+      <TopicButton topic={TOPICS.food} text="Food"/>
+      <TopicButton topic={TOPICS.math} text="Math"/>
+      <TopicButton topic={TOPICS.movies} text="Movies"/>
+      <TopicButton topic={TOPICS.sports} text="Sports"/>
     </div>
   )
 }
